4b7d8709976c0789c9de033cdb6056d419dc5938
Vinci Rufus <vinci.rufus@gmail.com>
2020-10-04 11:25:51 +0100

    micro apps setup

---
+++ apps/graph-ql/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'graph-ql',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  transform: {
+    '^.+\\.[tj]s$':  'ts-jest' 
+  },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/graph-ql'
+};

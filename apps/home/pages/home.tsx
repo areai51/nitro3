a6aa47fd1c0d9e4d28431e2ea8a715ae846d1fc1
Vinci Rufus <vinci.rufus@gmail.com>
2020-10-09 19:30:09 +0100

    script to build ngnix docker

---
+++ apps/home/pages/home.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import Grid from '@material-ui/core/Grid';
+
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { HeroBanner, ProductCard } from '@nitro3/components';
+import { Master as Layout } from '@nitro3/templates';
+import ProductMock from '../public/mocks/products-list.mock';
+
+const StyledPage = styled.div``;
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
+export const Index = () => {
+  const classes = useStyles();
+
+  return (
+    <Layout title="Nitro3: The Leading Microfrontend framework">
+      <div className={classes.heroContent}></div>
+      <HeroBanner
+        title="Microfrontends my Way"
+        description="Nitro3 is the 3rd Generation of Microfrontends."
+        image="images/nitro-banner.jpg"
+      />
+    </Layout>
+  );
+};
+
+export default Index;
